44a0fa1bfce952fa7234ab9e1f7260e1eca7a21f
Johan Filipsson <johan@domitor.se>
2016-09-29 11:36:21 +0200

    Add files via upload

--- Scripts/bloodline-1.0.0.js
+++ Scripts/bloodline-1.0.0.js
@@ -27,21 +27,21 @@ function registerBloodLineContainer(container_id) {
 }
 
 
-function createPerson(pos_x, pos_y, id, label) {
-    createObjectBox(pos_x, pos_y, id, label);
+function createPerson(pos_x, pos_y, id, label, person) {
+    createObjectBox(pos_x, pos_y, id, label, person);
     $("#" + id).data("PARTNER_RELID", '');
     $("#" + id).data("PARTNER_RELID", '');
     $("#" + id).data("HAS_RENDERED_CHILDREN", 'NO');
 }
 
-function createPartner(id, label, partner_id) {
+function createPartner(id, label, partner_id, person) {
     var partner = document.getElementById(partner_id);
     var rect = partner.getBoundingClientRect();
 
     var relationdiv_id = "PARTNER-" + id + "-" + partner_id;
 
     createRelationBox(rect.left + 220, rect.top + 26, relationdiv_id);
-    createObjectBox(rect.left + 290, rect.top, id, label);
+    createObjectBox(rect.left + 290, rect.top, id, label, person);
     createObjectLine(partner_id, relationdiv_id, "PARTNER_TO_PARTNER");
     createObjectLine(id, relationdiv_id, "PARTNER_TO_PARTNER");
 
@@ -52,7 +52,7 @@ function createPartner(id, label, partner_id) {
 
 }
 
-function createChild(id, label, mother_id, father_id) {
+function createChild(id, label, mother_id, father_id, person) {
     var parent = document.getElementById(mother_id) || document.getElementById(father_id);
     var rect = parent.getBoundingClientRect();
     var parent_partner_rel = $("#" + parent.id).data("PARTNER_RELID");
@@ -60,7 +60,7 @@ function createChild(id, label, mother_id, father_id) {
     //if (parent_has_rendered_children == "NO")
     //   last_x = 0;
 
-    createObjectBox(last_x + 270, rect.top + 110, id, label);
+    createObjectBox(last_x + 270, rect.top + 110, id, label, person);
     createObjectPath(id, parent_partner_rel, "CHILD_TO_PARENT");
 
     $("#" + parent.id).data("HAS_RENDERED_CHILDREN", "YES");
